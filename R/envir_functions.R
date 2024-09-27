8f884fdded7831c693379d96da2be04ac0bc5e41
allison-patterson <allison.patterson@ec.gc.ca>
2024-09-27 12:31:08 -0300

    wrap all functions that rely on bathymetry into get_bathymetry() functions Add shelfdist.

--- R/envir_functions.R
+++ R/envir_functions.R
@@ -2,120 +2,136 @@
 
 # get_bathymetry ----------------------------------------------------------
 
-#' Function to download bathymetry data
+#' Function to download bathymetry data and calculate related layers: slope, coastdist, shelfdist
 #'
 #' @param out_dir File path to the directory where raster will be saved. String.
-#' @param region Bounding box for region as xmin, ymin, xmax, ymax.
+#' @param region Bounding box for region as: xmin, xmax, ymin, ymax.
 #' @param overwrite Should existing files be overwritten. Logical.
 #' @param stride Rate at which to sample underlying raster resolution. Integer.
-#' @param mask_land Value of elevation that will be used to mask out land. Integer. Default is NULL.
-#' @param add_slope Should a layer for slope be calculated. Logical.
-#' @param neighbors Number of neighbours to use in slope calculation. Either 4 or 8.
-#' @param slope_unit Unit for the slope calculation. One of "degrees" or "radians".
-#' @param add_coastdist Should a layer for distance to coastline be calculated. Logical.
-#' @param dist_unit Unit for the distances One of "m" or "km".
+#' @param vars List of bathymetry related variables to calculate. Default is c('slope', 'coastdist','shelfdist').
+#' @param neighbors Number of neighbours to use in slope calculation. One of 4 or 8. Default is 8.
+#' @param slope_unit Unit for the slope calculation. One of "degrees" or "radians". Default is degrees.
+#' @param land_elev Value of elevation, in m, that will be used to mask out land when calculating coastdist. Integer. Default is 0
+#' @param shelf_elev Value of elevation, in m, that will be used to calculate distance
+#' @param dist_unit Unit for the distance values One of "m" or "km". Default is m
+#' when calculating coastdist. Integer. Default is -200 m
 #'
 #' @details
 #'
-#' This function uses rerddap to download the GEBCO_2020 Grid, which is a
-#' continuous terrain model for oceans and land at 15 arc-second intervals (0.004 deg).
+#' This function uses rerddap to download the GEBCO_2020 elevation grid, which is a
+#' continuous terrain model for oceans and land at 15 arc-second intervals (0.004 deg)
+#' with elevation values in meters. The stride argument is passed to rerddap if
+#' you want to downsample the resolution of the raster data before downloading.
+#' For example, a stride of 2 will only acquire every second grid cell and a stride
+#' of 10 will acquire every 10th grid cell.
 #'
-#' Stride is passed to rerddap if you want to downsample the resolution of the
-#' raster data before downloading. For example, a stride of 2 will only acquire
-#' every second grid cell and a stride of 10 will acquire every 10th grid cell.
+#' Vars is a list of common layers that can be derived from a bathymetry raster.
+#' Current options include slope, distance from coastline (coastdist), and distance from shelf break (shlefdist).
 #'
-#' If mask_land is not null, all elevation values greater than mask_land will be converted to NA.
+#' If vars includes slope, then slope is calculated using the terra::terrain() function.
+#' The arguments neighbors and slope_unit are arguments passed to terra::terrain()
+#' when calculating slope from a bathymetry layer. See ?terra::terrain for details.
 #'
-#' @return A netcdf file named 'bathymetry.nc' saved to out_dir. If add_slope or
-#' add_coast are set to TRUE, then additional files named 'slope.nc' and 'coastdist.nc'
-#' will be saved.
-#' @export
-#' @seealso [get_slope()], [get_coastdist()]
+#' If vars includes coastdist, then distance to coast is calculated the terra::costDist() function.
+#' The arguments land_elev and dist_unit are used when calculating coastdist.
+#' All elevation values greater than land_elev will be converted to NA and coastdist
+#' is calculated as distance to the closest NA value using terra::costDist(). The
+#' distance raster will be returned in dist_unit (m or km).
+#'
+#' If vars includes shelfdist, then distance to shelf break is calculated using
+#' the terra::costDist() function.The arguments shelf_elev and dist_unit are used
+#' when calculating shelfdist. All elevation values greater than shelf_elev (i.e.
+#' regions inshore of the shelf break) will be assigned negative distance values in dist_unit.
+#' All elevation values less than shelf_elev (i.e. regions offshore of the shelf
+#' break) will be assigned positive distance values in dist_unit. If shelf_elev is
+#' outside of the range of elevation values in the bathymetry layer an error will be returned.
+#'
+#' @note
+#'
+#' When calculating shelfdist, distances close to the edge of the raster layer may
+#' not reflect the true distance from the shelf break if the bathymetry layer is cropped.
+#'
+#' @return A netcdf file named 'bathymetry.nc' saved to out_dir. If additional
+#' variables are included using vars, then additional files named will be saved.
+#'
+#'@export
+#'
+#' @seealso [terra::terrain()], [terra::costDist()], [rerddap::griddap()]
 #'
 #' @examples
-#'# Just get bathymetry
+#'# Gets all variables
 #'get_bathymetry(out_dir = 'tmp',
-#'               region = c(-68, 44, -63, 46),
+#'               region = c(-68, -63, 44, 46),
 #'               overwrite = FALSE,
 #'               stride = 1,
-#'               mask_land = 0,
-#'               add_slope = FALSE,
+#'               vars = c('slope','coastdist','shelfdist'),
 #'               neighbors = 8,
 #'               slope_unit = c('degrees','radians'),
-#'               add_coastdist = FALSE,
-#'               dist_unit = c('m','km'))
+#'               land_elev = 0,
+#'               dist_unit = 'km',
+#'               shelf_elev = -50)
 #'r <- terra::rast('tmp/bathymetry.nc')
-#'terra::plot(r)
+#'terra::plot(r, main = terra::longnames(r))
+#'r <- terra::rast('tmp/slope.nc')
+#'terra::plot(r, main = terra::longnames(r))
+#'r <- terra::rast('tmp/coastdist.nc')
+#'terra::plot(r, main = terra::longnames(r))
+#'r <- terra::rast('tmp/shelfdist.nc')
+#'terra::plot(r, main = terra::longnames(r))
 #'
-#'# Get bathymetry and calculate slope and distance from coast
-#' get_bathymetry(out_dir = 'tmp',
-#'               region = c(-68, 44, -63, 46),
-#'               overwrite = FALSE,
-#'               stride = 1,
-#'               mask_land = 0,
-#'               add_slope = TRUE,
-#'               neighbors = 8,
-#'               slope_unit = 'degrees',
-#'               add_coastdist = TRUE,
-#'               dist_unit = 'km')
-#' r <- terra::rast('tmp/slope.nc')
-#' terra::plot(r, main = terra::longnames(r))
-#' r <- terra::rast('tmp/coastdist.nc')
-#' terra::plot(r, main = terra::longnames(r))
 #' unlink('tmp', recursive = TRUE)
 get_bathymetry <- function(out_dir = NULL,
                            region,
                            overwrite = FALSE,
                            stride = 1,
-                           mask_land = NULL,
-                           add_slope = FALSE,
+                           vars = c('slope','coastdist','shelfdist'),
                            neighbors = 8,
                            slope_unit = c('degrees','radians'),
-                           add_coastdist = FALSE,
+                           land_elev = 0,
+                           shelf_elev = -200,
                            dist_unit = c('m','km')
 ) {
 
   bath_file <- paste0(out_dir,'/bathymetry.nc')
 
-  if (file.exists(bath_file) == FALSE | overwrite == TRUE) {
-    b <- rerddap::griddap(rerddap::info('GEBCO_2020'),
-                          latitude = region[c(2,4)],
-                          longitude = region[c(1,3)],
-                          stride = stride,
-                          store = rerddap::disk(path = out_dir,
-                                                overwrite = TRUE)
-    )
-    file.rename(b$summary$filename, paste0(dirname(b$summary$filename), '/bathymetry.nc'))
-    message(paste('Saving:', bath_file))
-  } else message(paste('Skipping:', bath_file, 'exists in destination'))
-
-  if (is.null(mask_land) == FALSE) {
-    if (is.numeric(mask_land) == FALSE | length(mask_land) > 1) stop('mask_land must be a single numeric value')
-    r <- terra::rast(bath_file)
-    r <- terra::ifel(r > mask_land, NA, r)
-    terra::writeCDF(r, bath_file,
-                    longname = terra::longnames(r),
-                    unit = terra::units(r),
-                    varname = terra::varnames(r),
-                    overwrite = TRUE)
-    print(message('Masking elevation values greater than:', mask_land, terra::units(r)))
+  if (file.exists(bath_file) == TRUE & overwrite == FALSE) {
+    b <- terra::rast(bath_file)
+    bext <- terra::ext(b)
+    if (sum(sapply(1:4, function(x) round(bext[x]) == region[x]))!=4) {
+      warning('Extent of existing bathymetry file does not match region. Do you want to overwrite existing files?',call. = FALSE)
+    }
   }
 
-  if (add_slope) {
-    r <- terra::rast(bath_file)
+    if (file.exists(bath_file) == FALSE | overwrite == TRUE) {
+      b <- rerddap::griddap(rerddap::info('GEBCO_2020'),
+                            latitude = region[c(3,4)],
+                            longitude = region[c(1,2)],
+                            stride = stride,
+                            store = rerddap::disk(path = out_dir,
+                                                  overwrite = TRUE)
+      )
+      file.rename(b$summary$filename, paste0(dirname(b$summary$filename), '/bathymetry.nc'))
+      message(paste('Saving:', bath_file))
+    } else message(paste('Skipping:', bath_file, 'exists in destination'))
+
+  if ('slope' %in% vars) {
     get_slope(bath_file, out_dir, neighbors = neighbors, slope_unit = slope_unit, overwrite = overwrite)
   }
 
-  if (add_coastdist) {
-    r <- terra::rast(bath_file)
-    get_coastdist(bath_file, out_dir, mask_land = mask_land, dist_unit = dist_unit, overwrite = overwrite)
+  if ('coastdist' %in% vars) {
+    get_coastdist(bath_file, out_dir, land_elev = land_elev, dist_unit = dist_unit, overwrite = overwrite)
   }
+
+  if ('shelfdist' %in% vars) {
+    get_shelfdist(bath_file, out_dir, shelf_elev = shelf_elev, dist_unit = dist_unit, overwrite = overwrite)
+  }
+
 
 }
 
 # get_slope ---------------------------------------------------------------
 
-#' Function to calculate slope from a bathymetry file
+#' Internal function to calculate slope from a bathymetry file
 #'
 #' @param bath_file File path to saved bathymetry raster. String.
 #' @param out_dir File path to the directory where slope raster will be saved. String.
@@ -125,30 +141,14 @@ get_bathymetry <- function(out_dir = NULL,
 #'
 #' @details
 #' This function is a wrapper for terra::terrain(), which will calculate slope
-#' and save a standardized file to ensure consistency with other outputs from PelagicMagic.
-#' The function can be used directly with a bathymetry file as input or it can be galled with get_bathymetry()
-#' when doing the initial download of bathymetry data.
+#' and save a standardized file to ensure consistency with other outputs from get_bathymetry().
+#' This is an internal function called by get_bathymetry().
 #'
-#' See terra::terrain() for detailed description of how slope is calcualted.
+#' See terra::terrain() for detailed description of how slope is calculated.
 #'
 #' @return A netcdf file named 'slope.nc' saved to out_dir
-#' @export
 #' @seealso [get_bathymetry()], [terra::terrain()]
-#'
-#' @examples
-#'get_bathymetry(out_dir = 'tmp',
-#'               region = c(-68, 44, -63, 46),
-#'               overwrite = FALSE,
-#'               stride = 1,
-#'               mask_land = 0)
-#'get_slope(bath_file = 'tmp/bathymetry.nc',
-#'          out_dir = 'tmp',
-#'          neighbors = 8,
-#'          slope_unit = 'degrees',
-#'          overwrite = FALSE)
-#'r <- terra::rast('tmp/slope.nc')
-#'terra::plot(r)
-#'unlink('tmp', recursive = TRUE)
+#' @noRd
 #'
 get_slope <- function(bath_file,
                       out_dir,
@@ -157,7 +157,7 @@ get_slope <- function(bath_file,
                       overwrite = FALSE
                       ) {
 
-  if (file.exists(bath_file) == FALSE) stop(paste(bath_file), 'not found. Use get_bathymetry() to download bathymetry data.')
+  if (file.exists(bath_file) == FALSE) stop(paste(bath_file), ' not found. Add bathymetry to vars argument to download bathymetry data.')
   if (neighbors %in% c(4,8) == FALSE) stop('neighbours must be one of 4 or 8')
 
   b <- terra::rast(bath_file)
@@ -176,58 +176,40 @@ get_slope <- function(bath_file,
 
 # get_coastdist -----------------------------------------------------------
 
-#' Function to calculate slope from a bathymetry file
+#' Internal function to calculate slope from a bathymetry file
 #'
 #' @param bath_file File path to saved bathymetry raster. String.
 #' @param out_dir File path to the directory where slope raster will be saved. String.
-#' @param mask_land Value of elevation that will be used to mask out land. Integer. Default is 0
+#' @param land_elev Value of elevation that will be used to mask out land. Integer. Default is 0
 #' @param dist_unit Unit for the distances One of "m" or "km".
 #' @param overwrite Should existing files be overwritten. Logical.
 #'
 #' @details
-#' This function is a wrapper for terra::costDist(). This function requires a bathymetry
-#' file as input, where elevation values considered to be on land have been converted
-#' to NA.
-#'
-#' If the input raster includes on land elevations, then the mask_land argument,
-#' can be used to mask values greater than mask_land to NA.
+#' This function is a wrapper for terra::costDist(). This is an internal function
+#' called by get_bathymetry().
 #'
 #' @return A netcdf file named 'coastdist.nc' saved to out_dir
-#' @export
+#' @noRd
 #' @seealso [get_bathymetry()], [terra::costDist()]
-
 #'
-#' @examples
-#'get_bathymetry(out_dir = 'tmp',
-#'               region = c(-68, 44, -63, 46),
-#'               overwrite = FALSE,
-#'               stride = 1,
-#'               mask_land = 0)
-#'get_coastdist(bath_file = 'tmp/bathymetry.nc',
-#'              out_dir = 'tmp',
-#'              mask_land = 0,
-#'              dist_unit = 'km',
-#'              overwrite = TRUE)
-#'r <- terra::rast('tmp/coastdist.nc')
-#'terra::plot(r)
 get_coastdist <- function(bath_file,
                           out_dir,
-                          mask_land = 0,
+                          land_elev = 0,
                           dist_unit = c('m','km'),
                           overwrite = FALSE
                           ) {
-  if (file.exists(bath_file) == FALSE) stop(paste(bath_file), 'not found. Use get_bathymetry() to download bathymetry data.')
+  if (file.exists(bath_file) == FALSE) stop(paste(bath_file), ' not found. Add bathymetry to vars argument to download bathymetry data.')
 
   b <- terra::rast(bath_file)
-  if (is.null(mask_land) == FALSE) {
-    if (is.numeric(mask_land) == FALSE | length(mask_land) > 1) stop('mask_land must be a single numeric value')
-    b <- terra::ifel(b > mask_land, NA, b)
+  if (is.null(land_elev) == FALSE) {
+    if (is.numeric(land_elev) == FALSE | length(land_elev) > 1) stop('land_elev must be a single numeric value')
+    b <- terra::ifel(b > land_elev, NA, b)
   }
 
   coast_file <- paste0(out_dir,'/coastdist.nc')
   if (file.exists(coast_file) == FALSE | overwrite == TRUE) {
     count_na <- sum(is.na(terra::values(b, na.rm = F)))
-    if (sum(count_na) == 0) stop('Land values must be NA for get_coastdist(). Use mask_land argument to set land elevation.')
+    if (sum(count_na) == 0) stop('Land values must be NA for get_coastdist(). Use land_elev argument to set land elevation.')
     r <- terra::ifel(is.na(b), 0, 1)
     d <- terra::costDist(r, target = 0)
     if (dist_unit[1] == 'km') d <- d/1000
@@ -240,4 +222,59 @@ get_coastdist <- function(bath_file,
                     longname = paste0('distance from coast (',dist_unit[1],')'),
                     overwrite = T)
   } else (message(paste(coast_file, 'file already exists, skipping download')))
+}
+
+# get_coastdist -----------------------------------------------------------
+
+#' Internal function to calculate slope from a bathymetry file
+#'
+#' @param bath_file File path to saved bathymetry raster. String.
+#' @param out_dir File path to the directory where slope raster will be saved. String.
+#' @param land_elev Value of elevation that will be used to mask out land. Integer. Default is 0
+#' @param dist_unit Unit for the distances One of "m" or "km".
+#' @param overwrite Should existing files be overwritten. Logical.
+#'
+#' @details
+#' This function is a wrapper for terra::costDist(). This is an internal function
+#' called by get_bathymetry().
+#'
+#' @return A netcdf file named 'coastdist.nc' saved to out_dir
+#' @noRd
+#' @seealso [get_bathymetry()], [terra::costDist()]
+#'
+get_shelfdist <- function(bath_file,
+                          out_dir,
+                          shelf_elev = -200,
+                          dist_unit = c('m','km'),
+                          overwrite = FALSE
+) {
+
+  if (file.exists(bath_file) == FALSE) stop(paste(bath_file), ' not found. Add bathymetry to vars argument to download bathymetry data.')
+
+  b <- terra::rast(bath_file)
+
+  b_range <- range(terra::values(b, na.rm = T))
+  if (shelf_elev < b_range[1]) stop(paste0('shelf_elev [',shelf_elev,' m] is below minimum bathymetry [',b_range[1],' m]'))
+  if (shelf_elev > b_range[2]) stop(paste0('shelf_elev [',shelf_elev,' m] is above maximum bathymetry [',b_range[1],' m]'))
+
+
+  shelf_file <- paste0(out_dir,'/shelfdist.nc')
+
+  if (file.exists(shelf_file) == FALSE | overwrite == TRUE) {
+
+    rn <- terra::ifel(b < shelf_elev, 0, 1)
+    inshore <- terra::costDist(rn, target = 0)
+    if (dist_unit[1] == 'km') inshore <- inshore/1000
+    inshore <- -inshore
+    rn <- terra::ifel(b > shelf_elev, 0, 1)
+    offshore <- terra::costDist(rn, target = 0)
+    if (dist_unit[1] == 'km') offshore <- offshore/1000
+    d <- inshore + offshore
+    names(d) <- 'shelfdist'
+    terra::writeCDF(d, shelf_file,
+                    unit = dist_unit[1],
+                    varname = 'shelfdist',
+                    longname = paste0('Distance from shelf break [', shelf_elev,' m] (',dist_unit[1],')'),
+                    overwrite = T)
+  } else (message(paste(shelf_file, 'file already exists, skipping download')))
 }
